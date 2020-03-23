5b3856324d81b2a137794ab2b8ca9689093cc858
rageshwari <ragu.bodke@gmail.com>
2020-03-23 21:29:58 +0530

    The player checks for options-No_PLAY,LADDER,SNAKE

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -1,9 +1,38 @@
 #!/bin/bash -x
 #Program for Snake and Ladder Simulator
 #Variables
+POSITION=0
 position=0
+NO_PLAY=0
+LADDER=1
+SNAKE=2
+ladderCount=0
+snakeCount=0
 echo "Welcome to Snake Ladder Game"
 
 #Roll a die to get random number between 1 to 6
 dieOutcome=$((RANDOM%6+1))
-echo $dieOutcome
+echo "Rolling die Outcome is::" $dieOutcome
+
+#Check for options
+optionCheck=$((RANDOM%3))
+case $optionCheck in
+	$NO_PLAY)
+		echo "Current Position::" $position
+		echo "You stays in a same position::"$position ;;
+	$LADDER)
+		((ladderCount++))
+		echo "Current Position::" $position
+		position=$((position+dieOutcome))
+		echo "Your encountered a Ladder...New position::" $position ;;
+	$SNAKE)
+		((snakeCount++))
+		echo "current Position::" $position
+		position=$((position-dieOutcome)) 
+		if (( $position < 0 ))
+		then
+			echo "You encountered a snake...New position::" $POSITION
+		else
+			echo "You encountered a snake...New position::" $position
+		fi
+esac
